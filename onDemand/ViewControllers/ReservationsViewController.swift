18473b40a6c61128fe446fe6354c18984a73af4e
Mayank Gandhi <mayankgandhi50@gmail.com>
2020-12-20 21:41:20 -0800

    Add Custom VC files, Navigation Bar -> Links

---
+++ onDemand/ViewControllers/ReservationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReservationsViewController.swift
+//  onDemand
+//
+//  Created by Mayank Gandhi on 12/20/20.
+//
+
+import UIKit
+
+class ReservationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      navigationController?.navigationBar.prefersLargeTitles = true
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
