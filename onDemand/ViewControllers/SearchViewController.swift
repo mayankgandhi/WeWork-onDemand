fb9896457387c9a2e36a1c82b007460f5d24f297
Mayank Gandhi <mayankgandhi50@gmail.com>
2020-12-21 04:26:54 -0800

    cleaner

--- onDemand/ViewControllers/SearchViewController.swift
+++ onDemand/ViewControllers/SearchViewController.swift
@@ -9,67 +9,67 @@ import MapKit
 import UIKit
 
 class SearchViewController: UIViewController, UICollectionViewDelegate {
-    var coordinator: SearchCoordinator?
-
-    @IBOutlet var searchCollectionView: UICollectionView!
-
-    @IBAction func propertyTypeChanged(_ sender: UISegmentedControl) {
-        print(#function)
-        performQuery(of: sender.selectedSegmentIndex == 0 ? .location : .room)
-    }
-
-    @IBOutlet var mapView: MKMapView!
-    var annotations = [MapProperty]()
-
-    enum Section: CaseIterable {
-        case main
-    }
-
-    var dataSource: UICollectionViewDiffableDataSource<Section, Property>!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        searchCollectionView.delegate = self
-        configureDataSource()
-        performQuery()
-        coordinator = SearchCoordinator(navigationController: navigationController!)
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
+  var coordinator: SearchCoordinator?
+  
+  @IBOutlet var searchCollectionView: UICollectionView!
+  
+  @IBAction func propertyTypeChanged(_ sender: UISegmentedControl) {
+    print(#function)
+    performQuery(of: sender.selectedSegmentIndex == 0 ? .location : .room)
+  }
+  
+  @IBOutlet var mapView: MKMapView!
+  var annotations = [MapProperty]()
+  
+  enum Section: CaseIterable {
+    case main
+  }
+  
+  var dataSource: UICollectionViewDiffableDataSource<Section, Property>!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    searchCollectionView.delegate = self
+    configureDataSource()
+    performQuery()
+    coordinator = SearchCoordinator(navigationController: navigationController!)
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+  }
 }
 
 extension SearchViewController {
-    func configureDataSource() {
-        dataSource = UICollectionViewDiffableDataSource<Section, Property>(collectionView: searchCollectionView, cellProvider: { (collectionView, indexPath, property) -> UICollectionViewCell? in
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PropertyCell", for: indexPath) as! PropertyCell
-            cell.configure(with: property)
-            return cell
-        })
+  func configureDataSource() {
+    dataSource = UICollectionViewDiffableDataSource<Section, Property>(collectionView: searchCollectionView, cellProvider: { (collectionView, indexPath, property) -> UICollectionViewCell? in
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PropertyCell", for: indexPath) as! PropertyCell
+      cell.configure(with: property)
+      return cell
+    })
+  }
+  
+  func performQuery(of type: PropertyType? = .location) {
+    let properties = Property.allProperties.filter { (prop) -> Bool in
+      prop.type == type
     }
-
-    func performQuery(of type: PropertyType? = .location) {
-        let properties = Property.allProperties.filter { (prop) -> Bool in
-            prop.type == type
-        }
-        mapView.removeAnnotations(annotations)
-        annotations.removeAll()
-        properties.forEach { prop in
-            annotations.append(prop.getAnnotation())
-        }
-        mapView.addAnnotations(annotations)
-        mapView.showAnnotations(annotations, animated: true)
-
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Property>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(properties)
-        dataSource.apply(snapshot, animatingDifferences: true)
+    mapView.removeAnnotations(annotations)
+    annotations.removeAll()
+    properties.forEach { prop in
+      annotations.append(prop.getAnnotation())
     }
-
-    func collectionView(_: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let propertySelected: Property = dataSource.itemIdentifier(for: indexPath)!
-        coordinator?.showDetail(for: propertySelected)
-    }
+    mapView.addAnnotations(annotations)
+    mapView.showAnnotations(annotations, animated: true)
+    
+    var snapshot = NSDiffableDataSourceSnapshot<Section, Property>()
+    snapshot.appendSections([.main])
+    snapshot.appendItems(properties)
+    dataSource.apply(snapshot, animatingDifferences: true)
+  }
+  
+  func collectionView(_: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    let propertySelected: Property = dataSource.itemIdentifier(for: indexPath)!
+    coordinator?.showDetail(for: propertySelected)
+  }
 }
